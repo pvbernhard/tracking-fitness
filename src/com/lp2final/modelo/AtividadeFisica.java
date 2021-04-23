cdc434a66b2ec8d1720fbe79b340be361c97af8d
pvbernhard <pvbernhard@gmail.com>
2021-04-23 17:47:58 -0300

    criar AtividadeFisica

--- src/com/lp2final/modelo/AtividadeFisica.java
+++ src/com/lp2final/modelo/AtividadeFisica.java
@@ -1,4 +1,44 @@
 package com.lp2final.modelo;
 
-public class AtividadeFisica {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class AtividadeFisica implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private Integer segundos;
+    private Double taxaCalorias;
+
+    public AtividadeFisica(String nome, Double taxaCalorias) {
+        this.nome = nome;
+        this.taxaCalorias = taxaCalorias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(Integer segundos) {
+        this.segundos = segundos;
+    }
+
+    public Double getTaxaCalorias() {
+        return taxaCalorias;
+    }
+
+    @Override
+    public String toString() {
+        return "AtividadeFisica{" +
+                "nome='" + nome + '\'' +
+                ", segundos=" + segundos +
+                ", taxaCalorias=" + taxaCalorias +
+                '}';
+    }
 }
