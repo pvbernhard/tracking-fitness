5c4cf2b8e33cb9f1e0c5569272d11e21ed922e29
Mario <marioffreitas.mf@gmail.com>
2021-04-24 04:04:01 -0300

    Faz funcoes relacionadas ao peso

--- src/com/lp2final/controle/PerfilControle.java
+++ src/com/lp2final/controle/PerfilControle.java
@@ -2,23 +2,37 @@ package com.lp2final.controle;
 
 import com.lp2final.modelo.Perfil;
 
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class PerfilControle {
 
     private final String PASTA = "config";
     private final String nome = "Perfil.dat";
+    private final String nomePeso = "PerfilPeso.dat";
 
-    Path caminho = Paths.get(this.PASTA, this.nome);
+    private Path caminho = Paths.get(this.PASTA, this.nome);
+    private Path caminhoPeso = Paths.get(this.PASTA,this.nomePeso);
 
     public boolean perfilExist(){
         if(Files.exists( caminho )){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public boolean perfilPesoExist(){
+        if(Files.exists( caminhoPeso )){
             return true;
         }else{
             return false;
@@ -193,6 +207,8 @@ public class PerfilControle {
                         sub = sub.replace(",",".");
                         a.editarPerfil("Peso", sub);
                         perfil.setPeso(Double.valueOf(sub).doubleValue());
+                        a.perdaDePesoTemp(sub);
+
                     }
                 }
             }
@@ -248,6 +264,193 @@ public class PerfilControle {
 
     }
 
+    private boolean dataRepetida(){
+
+        BufferedReader conteudo = null;
+        String linha = "";
+        ArrayList<String> perfil = new ArrayList<>();
+
+        try {
+
+            conteudo = new BufferedReader(new FileReader(caminhoPeso.toFile()));
+
+            while ((linha = conteudo.readLine()) != null) {
+
+                String dados[] = linha.split(";");
+
+                perfil.add(dados[0]);
+
+            }
+
+            for (int i = 0;i<perfil.size();i++){
+
+                for(int j = 0; i<perfil.size();i++){
+                    if(perfil.get(i) == perfil.get(j)){
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+        } catch (
+                FileNotFoundException e) {
+            System.out.println("Arquivo nao encontrado:" + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("IndexOutBouds: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Erro: " + e.getMessage());
+        } finally {
+            if (conteudo != null) {
+                try {
+                    conteudo.close();
+                } catch (IOException e) {
+                    System.out.println("IO Erro: " + e.getMessage());
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean editaPeso(String novo){
+        BufferedReader conteudo = null;
+        String linha = "";
+        String totalL ="";
+        boolean uma = true;
+        int qlinhas = 0;
+
+
+        try (FileWriter criar = new FileWriter(caminhoPeso.toFile(), true);
+             BufferedWriter buffer = new BufferedWriter(criar);
+             PrintWriter escrever = new PrintWriter(buffer);) {
+
+            conteudo = new BufferedReader(new FileReader(caminhoPeso.toFile()));
+            conteudo.mark(1000);
+            ArrayList<String> salvar = new ArrayList<>();
+            ArrayList<String> rep = new ArrayList<>();
+
+            while ((totalL = conteudo.readLine()) != null) {
+                qlinhas++;
+            }
+            conteudo.reset();
+            while ((linha = conteudo.readLine()) != null) {
+                // salva tudo dentro de um arrayList+
+                String[] dados = linha.split(";");
+
+                if(qlinhas == 1){
+                    dados[1] = novo;
+                    String linhaAlterada = dados[0]+";"+dados[1];
+                    salvar.add(linhaAlterada);
+                    continue;
+                }
+
+                if(uma){
+                    rep.add(dados[0]);
+                    uma = false;
+                    String linhaAlterada = dados[0]+";"+dados[1];
+                    salvar.add(linhaAlterada);
+                    continue;
+                }
+
+                for(int i = 0;i<rep.size();i++){
+                    if(dados[0].equals(rep.get(i))){
+                        dados[1] = novo;
+                    }
+                }
+
+                String linhaAlterada = dados[0]+";"+dados[1];
+                salvar.add(linhaAlterada);
+
+            }
+            criar.close();
+            buffer.close();
+            escrever.close();
+
+            // apaga tudo q tinha no arquivo
+            FileWriter apg = new FileWriter(caminhoPeso.toFile(), true);
+            apg.close();
+
+            // escrever tudo no novo arquivo
+            FileWriter cNovo = new FileWriter(caminhoPeso.toFile());
+            BufferedWriter newBuffer = new BufferedWriter(cNovo);
+
+            for (int i = 0; i < salvar.size(); i++) {
+
+                newBuffer.write(salvar.get(i));
+                newBuffer.newLine();
+
+            }
+
+            newBuffer.close();
+            cNovo.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo nao encontrado:" + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("IndexOutBouds: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Erro: " + e.getMessage());
+        } finally {
+            if (conteudo != null) {
+                try {
+                    conteudo.close();
+                } catch (IOException e) {
+                    System.out.println("IO Erro: " + e.getMessage());
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+    public void perdaDePesoTemp(String peso){
+
+        BufferedReader conteudo = null;
+        String escreverNoArq = "";
+        Date data = new Date();
+        Calendar d = Calendar.getInstance();
+
+        try (FileWriter criar = new FileWriter(caminhoPeso.toFile(), true);
+             BufferedWriter buffer = new BufferedWriter(criar);
+             PrintWriter escrever = new PrintWriter(buffer);) {
+
+            if(perfilPesoExist()){
+                if(dataRepetida()){
+                    editaPeso(peso);
+                }else {
+
+                    String dia = d.get(5)+"";
+                    String mes = d.get(2)+1 +"";
+                    String ano = d.get(1)+"";
+
+                    String dataAgora = dia+":"+mes+":"+ano;
+
+                    escreverNoArq = dataAgora +";"+peso+"\n";
+                    escrever.append(escreverNoArq);
+
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo nao encontrado:" + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("IndexOutBouds: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Erro: " + e.getMessage());
+        } finally {
+            if (conteudo != null) {
+                try {
+                    conteudo.close();
+                } catch (IOException e) {
+                    System.out.println("IO Erro: " + e.getMessage());
+                }
+            }
+        }
+
+    }
 
     public void criarArq(){
 
