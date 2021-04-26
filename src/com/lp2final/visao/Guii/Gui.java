0ea73e3398f4e6bda9699217f9bcaa1d8c7e0321
Mario <marioffreitas.mf@gmail.com>
2021-04-25 22:17:15 -0300

    Faz criar Perfil

--- src/com/lp2final/visao/Guii/Gui.java
+++ src/com/lp2final/visao/Guii/Gui.java
@@ -1,4 +1,4 @@
-package com.lp2final.visao;
+package com.lp2final.visao.Guii;
 
 import com.lp2final.controle.PerfilControle;
 import com.lp2final.modelo.Perfil;
@@ -108,88 +108,7 @@ public class Gui extends JFrame {
         //adiciona cade painel em seua posiçoes podem ser (NORTH, SOUTH,WEST,EAST,CENTER)
 
     }
-    //criaçao do perfil
 
-    Dimension tamTela = getToolkit().getScreenSize();
-    int largT = tamTela.width;
-    int altT = tamTela.height;
-
-    int larg = (int) (largT*0.3);
-    int alt = (int) (altT*0.5);
-
-    public Gui(boolean menu){
-        super("Menu");
-        menu = true;
-        criarMenuSUperiorMenu();
-        criarMenu();
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(larg, alt);
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
-
-    public void criarMenuSUperiorMenu(){
-
-        JMenu menuSuperior = new JMenu("Mais");//barra superior
-
-        JMenuItem menuItem = new JMenuItem("Sobre");//informaçoes que iriam aparece quando aberta o menu
-
-        menuSuperior.add(menuItem);//adiciona o sobre dentro do mais
-
-
-        JMenuItem menuDoidao = new JMenuItem("Doidao");
-        menuSuperior.add(menuItem);
-
-        JMenuBar barra = new JMenuBar();//mostra onde ficaria o menu criado a cima
-        setJMenuBar(barra);
-        barra.add(menuSuperior);
-    }
-
-    public void criarMenu(){
-
-        setLayout(new BorderLayout());//criar um borda em torno da janela
-
-        //titulo
-        JPanel panelTitulo = new JPanel();
-        panelTitulo.setLayout(new FlowLayout());//criar um layout flow
-        JLabel titulo = new JLabel("Menu Principal");
-
-        Font f = new Font("Verdana", Font.PLAIN,16);//Criar uma fonte -> nome da fonte/o formato regular/tamanho
-        titulo.setFont(f);//adiciona a fonte no titulo
-
-        panelTitulo.add(titulo);//adiciona informaçoes no painel
-        //titulo
-
-        //Painel onde o usario botara as informaçoes
-        JPanel panelBoteoes = new JPanel();
-        panelBoteoes.setLayout(null);
-
-        //botoens
-        JButton botaoPerfil = new JButton("Perfil");
-        JButton botaoExercicios = new JButton("Lista De Exercicios");
-        JButton botaoRelatorio = new JButton("Relatorio");
-        JButton botaoPdf = new JButton("Gerar PDF");
-
-        //tamanho e local dos botoes
-        botaoPerfil.setBounds( (larg/2) - 150/2,10+70*0,150,60);
-        botaoExercicios.setBounds((larg/2) - 150/2,10+70*1,150,60);
-        botaoRelatorio.setBounds((larg/2) - 150/2,10+70*2,150,60);
-        botaoPdf.setBounds((larg/2) - 150/2,10+70*3,150,60);
-
-        panelBoteoes.add(botaoPerfil);
-        panelBoteoes.add(botaoExercicios);
-        panelBoteoes.add(botaoRelatorio);
-        panelBoteoes.add(botaoPdf);
-
-
-        //adiciona cade painel em seua posiçoes podem ser (NORTH, SOUTH,WEST,EAST,CENTER)
-        add(panelTitulo ,BorderLayout.NORTH );
-        add(panelBoteoes , BorderLayout.CENTER);
-
-        //adiciona cade painel em seua posiçoes podem ser (NORTH, SOUTH,WEST,EAST,CENTER)
-
-    }
 
 
 
@@ -242,11 +161,14 @@ public class Gui extends JFrame {
 
                 JOptionPane.showMessageDialog(null,"Cadastro Concluido","Cadastro",JOptionPane.PLAIN_MESSAGE);
                 p.criarArq(perfil,p);
-                Gui g = new Gui(true);
+                GuiMenu g = new GuiMenu();
                 dispose();
             }
 
         }
     }
+
+
+
 
 }
